b69bd231d1f2065c573fa7319dbc0230c3556577
Bryan Lee <pygmize.me@gmail.com>
2015-02-25 02:21:34 -0800

    Added _.uniq for unsorted array

--- src/underbar.js
+++ src/underbar.js
@@ -81,16 +81,45 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    
+    _.each(collection, function(value){
+      if (test(value) == true)
+        result.push(value);
+    });
+    
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(value){
+      return !test(value);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var unique = [];
+    var sorted;
+    
+    if (arguments[1] != undefined)
+      sorted = arguments[1];
+    else
+      sorted = false;
+    if (arguments[2] != undefined)
+      var iterator = arguments[2];
+     
+    if (sorted == false){
+      _.each(array, function(value){
+        if (_.indexOf(unique, value) == -1)
+          unique.push(value);
+      });
+    }
+    
+    return unique;
   };
 
 
